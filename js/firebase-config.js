9e6087b1c5164833017cbb37295de1944795258e
tanmay316 <sharmamoni913@gmail.com>
2026-07-12 19:05:54 +0530

    Configure index.html entrypoint and Firebase database settings

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4PfAeIz76YYjNWEWix8xk_COM6mGrVcs",
+  authDomain: "apparatus-46b1b.firebaseapp.com",
+  projectId: "apparatus-46b1b",
+  storageBucket: "apparatus-46b1b.firebasestorage.app",
+  messagingSenderId: "716398124057",
+  appId: "1:716398124057:web:8e8c4c8d31ea073067de88",
+  measurementId: "G-YJB1454STJ"
+};
