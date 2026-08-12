acd766cf94dd8cac52600b8a3a41999ef8c37da6
tanmay316 <sharmamoni913@gmail.com>
2026-08-12 10:49:23 +0530

    OTA

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,21 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.tms.apparatus',
+  appId: 'com.apparatus.apparatus',
   appName: 'Apparatus',
   webDir: 'dist',
   plugins: {
     CapacitorUpdater: {
       autoUpdate: true,
+      appId: 'com.apparatus.apparatus'
     },
     GoogleAuth: {
-      scopes: ['profile', 'email'],
+      scopes: [
+        'profile',
+        'email'
+      ],
       serverClientId: '716398124057-hhg54cto4lnft33chuh0gmb5ofkp4qki.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
+      forceCodeForRefreshToken: true
     },
     SplashScreen: {
       launchAutoHide: false,
